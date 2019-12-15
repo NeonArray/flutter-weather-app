d6eb4a0c3d5b162d708be662263643f9becb5e61
Aaron <github@otacon.press>
2019-12-14 22:32:34 -0500

    Refactor methods in weather service class

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -5,20 +5,23 @@ const kAPIkey = '6598e125ef69710dbc8b44955879129c';
 const kOpenWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
-  Future<dynamic> getCityWeather(String city) async {
-    var url = '$kOpenWeatherMapURL?q=$city&appid=$kAPIkey&units=imperial';
+  Future<dynamic> getWeather(String url) async {
     return await NetworkHelper(url).getData();
+  }
+
+  dynamic getCityWeather(String city) {
+    var url = '$kOpenWeatherMapURL?q=$city&appid=$kAPIkey&units=imperial';
+    return getWeather(url);
   }
 
   Future<dynamic> getLocationWeather() async {
     var location = Location();
     await location.getCurrentLocation();
 
-    NetworkHelper networkHelper = NetworkHelper(
-      '$kOpenWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$kAPIkey&units=imperial',
-    );
+    var url =
+        '$kOpenWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$kAPIkey&units=imperial';
 
-    return await networkHelper.getData();
+    return getWeather(url);
   }
 
   String getWeatherIcon(int condition) {
