c208e21a6b6efcbfdba6ac4d67731ae7e66a8f0e
Aaron <github@otacon.press>
2019-12-14 22:34:41 -0500

    Refactored logic into weather service

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,11 +1,8 @@
-import 'package:clima/services/location.dart';
-import 'package:clima/services/networking.dart';
+import 'package:clima/services/weather.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 import 'location_screen.dart';
-
-const kAPIkey = '6598e125ef69710dbc8b44955879129c';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -13,9 +10,6 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  double _latitude;
-  double _longitude;
-
   @override
   void initState() {
     super.initState();
@@ -23,19 +17,13 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void _getLocationData() async {
-    var location = Location();
-    await location.getCurrentLocation();
-
-    _latitude = location.latitude;
-    _longitude = location.longitude;
-
-    NetworkHelper networkHelper = NetworkHelper(
-        'https://samples.openweathermap.org/data/2.5/weather?lat=$_latitude&lon=$_longitude&appid=$kAPIkey');
-    var data = await networkHelper.getData();
+    var weatherData = await WeatherModel().getLocationWeather();
 
     Navigator.push(context, MaterialPageRoute(
       builder: (context) {
-        return LocationScreen();
+        return LocationScreen(
+          locationWeather: weatherData,
+        );
       },
     ));
   }
