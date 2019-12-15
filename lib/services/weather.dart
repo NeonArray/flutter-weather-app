915db676e4316d69b83517e63a099dd1f765a46d
Aaron <github@otacon.press>
2019-12-14 22:32:16 -0500

    Add methods to weather service

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,26 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+const kAPIkey = '6598e125ef69710dbc8b44955879129c';
+const kOpenWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+  Future<dynamic> getCityWeather(String city) async {
+    var url = '$kOpenWeatherMapURL?q=$city&appid=$kAPIkey&units=imperial';
+    return await NetworkHelper(url).getData();
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    var location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+      '$kOpenWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$kAPIkey&units=imperial',
+    );
+
+    return await networkHelper.getData();
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
